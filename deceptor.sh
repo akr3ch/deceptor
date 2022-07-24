0edf9855bcb2acfe4c8a1b991619ad88aed48f93
akrecH <97300177+akr3ch@users.noreply.github.com>
2022-07-24 13:00:49 +0600

    Update deceptor.sh

--- deceptor.sh
+++ deceptor.sh
@@ -3,7 +3,7 @@
 ##### Web Cache Deception exploitation script ####
 ## coder: akrech				##
 ## github: https://github.com/akr3ch		##
-## website: akech.xyz   			##
+## website: akrech.xyz   			##
 ## twitter: a_k_r_e_c_H				##
 ##################################################
 
